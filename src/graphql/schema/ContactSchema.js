1eafe1c7918872a543ec9f9b9cd31752ab33de35
felipe.domingues <felipe.domingues@calix.com>
2022-04-15 10:36:03 -0300

    Making simple query with graphql

--- src/graphql/schema/ContactSchema.js
+++ src/graphql/schema/ContactSchema.js
@@ -1,3 +1,16 @@
-const ContactSchema = () => {};
+import { buildSchema } from "graphql";
+const ContactSchema = buildSchema(`
+type Contact {
+  _id: String
+  picture: String
+  birthday: String
+  name: String
+  address: String
+  phone_number: String }
+  
+type Query {
+  contact(_id:String): Contact
+}
+`);
 
 export { ContactSchema };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import { graphqlHTTP } from "express-graphql";
 import { ContactSchema } from "./graphql";
+import db from "./data/contacts.json";
 
 const app = express();
+app.use(cors());
 
-app.use("/graphql", graphqlHTTP({ ContactSchema }));
+const root = {
+  contact: ({ _id }) => {
+    return db.find((contact) => contact._id === _id);
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: ContactSchema,
+    graphiql: process.env.NODE_ENV === "dev",
+    rootValue: root,
+  })
+);
 
 app.listen(process.env.PORT, () =>
   console.log(`Listening on port:  ${process.env.PORT}`)
