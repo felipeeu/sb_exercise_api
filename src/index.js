c33e4c69df8ec22105076db0a4ad5711ba8eefe9
felipe.domingues <felipe.domingues@calix.com>
2022-04-16 08:11:26 -0300

    Remove root value from middleware

--- src/index.js
+++ src/index.js
@@ -3,23 +3,15 @@ import express from "express";
 import cors from "cors";
 import { graphqlHTTP } from "express-graphql";
 import { schema } from "./schema";
-import db from "./data/contacts.json";
 
 const app = express();
 app.use(cors());
-
-const root = {
-  contact: ({ _id }) => {
-    return db.find((contact) => contact._id === _id);
-  },
-};
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "dev",
-    rootValue: root,
   })
 );
 
