5c887bd39bc5959603612243fc25999dae2b0fc8
felipe.domingues <felipe.domingues@calix.com>
2022-04-17 08:26:28 -0300

    Removing mutation from schema

--- src/schema.js
+++ src/schema.js
@@ -1,8 +1,6 @@
 import { GraphQLSchema } from "graphql";
 import { Query } from "./type/QueryType";
-import { Mutation } from "./type/MutationType";
 
 export const schema = new GraphQLSchema({
   query: Query,
-  mutation: Mutation,
 });
