31882f482b5299e81e9726fd2fa8fe2b3161b6f8
felipe.domingues <felipe.domingues@calix.com>
2022-04-18 17:19:18 -0300

    Import resolver in the root query

--- src/type/QueryType.js
+++ src/type/QueryType.js
@@ -1,6 +1,8 @@
 import { GraphQLObjectType, GraphQLList } from "graphql";
 import { Contact } from "./ContactType";
 import { ContactsInputFilter } from "./ContactsInputFilter";
+import { contactFilters } from "../resolvers";
+import db from "../data/contacts.json";
 
 const Query = new GraphQLObjectType({
   name: "Query",
@@ -12,6 +14,7 @@ const Query = new GraphQLObjectType({
       args: {
         filter: { type: ContactsInputFilter },
       },
+      resolve: (_, args) => contactFilters(db, args),
     },
   }),
 });
